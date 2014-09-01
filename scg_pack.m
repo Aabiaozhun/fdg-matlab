a9ce2728167bc7c9985176f21f1597d28df7947b
imwangxu <i@wangxu.im>
2014-09-01 17:20:09 +0200

    delete useless files
    packing sc and scg is not used anymore.

--- scg_pack.m
+++
@@ -1,9 +0,0 @@
-function [ scg ] = scg_pack( scs, graph, itosc, sctoi )
-
-scg.scs = scs;
-scg.graph = graph;
-scg.itosc = itosc;
-scg.sctoi = sctoi;
-
-end
-
--- scg_unpack.m
+++
@@ -1,9 +0,0 @@
-function [ scs, graph, itosc, sctoi ] = scg_unpack( scg )
-
-scs = scg.scs;
-graph = scg.graph;
-itosc = scg.itosc;
-sctoi = scg.sctoi;
-
-end
-
